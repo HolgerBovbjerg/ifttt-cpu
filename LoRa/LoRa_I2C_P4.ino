5845e6ed332f775fdce6de36a32be4b383156458
Morten Janner <morten_janner@hotmail.com>
2019-05-13 11:13:31 +0200

    Update LoRa_I2C_P4

--- LoRa/LoRa_I2C_P4.ino
+++ LoRa/LoRa_I2C_P4.ino
@@ -8,19 +8,15 @@
 byte SlaveReceived = 0;
 byte SlaveSend = 0;
 int DataToMaster = 0;
-String fff;
+
 int aaa = 0;
 int bbb = 0;
-int ccc = 0;
-int eee = 0;
+
 int DataToSend = 0;
 int DataToReceive = 0;
-int DataAvailable = 2;
-char *str = "hej";
-int *ptr;
-char array1[] = {0,0,0,0,0,0,0,0};
+int DataAvailable = 1;
+
 int array2[] = {233,8,1,0,0,0,0,0};
-int array4[] = {0,0,0,0,0,0,0,0};
 
 void setup()
 
@@ -45,10 +41,11 @@ void setup()
 
 void loop()
 {
-  aaa = (digitalRead(4));  //Reads Acknowledge bit
+  aaa = (digitalRead(4));                                                                //Reads Acknowledge bit
 
   if (DataAvailable > 0) {
-    
+
+    /*
     Serial.print("array2 [0]: ");
     Serial.println(array2[0]);
     Serial.print("array2 BIN [0]: ");
@@ -77,8 +74,8 @@ void loop()
     
     Serial.print("SlaveSend BIN: ");
     Serial.println(SlaveSend,BIN);
-    
-    bitWrite(SlaveSend, 0,(bitRead(array2[0],0)));
+    */
+    bitWrite(SlaveSend, 0,(bitRead(array2[0],0)));                                     //Changes the bits from Data receeved data to SlaveSend one bit at the time.
     bitWrite(SlaveSend, 1,(bitRead(array2[0],1)));
     bitWrite(SlaveSend, 2,(bitRead(array2[0],2)));
     bitWrite(SlaveSend, 3,(bitRead(array2[0],3)));
@@ -86,30 +83,30 @@ void loop()
     bitWrite(SlaveSend, 5,(bitRead(array2[0],5)));
     bitWrite(SlaveSend, 6,(bitRead(array2[0],6)));
     bitWrite(SlaveSend, 7,(bitRead(array2[0],7)));
-    
+    /*
     Serial.print("SlaveSend: ");
     Serial.println(SlaveSend);
     Serial.print("SlaveSend BIN: ");
     Serial.println(SlaveSend,BIN);
-    
-    digitalWrite(5, HIGH);
+    */
+    digitalWrite(5, HIGH);                                                             //sets interupt High
     
     delay(20);
   
-    DataAvailable = 0; //skal fjernes når der skal næses over i2c
+    DataAvailable = 0;                                                                 //skal fjernes når der skal næses over i2c !!!!!!!!!!!!!!!!!!!!!!!!!!
   
   }
-  if (aaa == HIGH){ //if Acknowledge HIGH 
-    digitalWrite(5,LOW);
-    //DataAvailable = 0;
+  if (aaa == HIGH){                                                                    //if Acknowledge HIGH
+    digitalWrite(5,LOW);                                                               //set interupt low
+    //DataAvailable = 0;                                                               //set data available to 0 so taht interupt do not get higt again
   }
 
-  if (bbb == 1){ //hvis bbb er 1, så er der modtaget data fra master over i2c.
+  if (bbb == 1){                                                                       //hvis bbb er 1, så er der modtaget data fra master over i2c.
     Serial.print("Sending packet: ");
-    DataToSend = 1; //sættes til 1 for at sende data
+    DataToSend = 1;                                                                    //sættes til 1 for at sende data
     // read packet
 
-    while (DataToSend > 0)//(LoRa.available())
+    while (DataToSend > 0)
     {
       
       // send packet
@@ -119,17 +116,17 @@ void loop()
       
       delay(1000);
 
-      DataToSend = 0; // sættes til nul når pakken er sendt
-      bbb = 0; //sættes til 0 når bakken er sendt
+      DataToSend = 0;                                                                   // sættes til nul når pakken er sendt
+      bbb = 0;                                                                          //sættes til 0 når bakken er sendt
     }
   }
 
-  if (bbb == 0){ //let the LoRa recive when it is not sending.
+  if (bbb == 0){                                                                        //let the LoRa recive when it is not sending.
     int packetSize = LoRa.parsePacket();
     if (packetSize) {
       // received a packet
       Serial.println("Received packet: ");
-      DataToReceive = 1;
+      DataToReceive = 1;                                                                //DataToReceive is set to 1 to continiue the while loop.
       // read packet
 
       while (DataToReceive > 0)
@@ -145,13 +142,13 @@ void loop()
 }
 
 
-void receiveEvent (int howMany)                    //This Function is called when Slave receives value from master
+void receiveEvent (int howMany)                                                        //This Function is called when Slave receives value from master
   {
-  SlaveReceived = Wire.read();                    //Used to read value received from master and store in variable SlaveReceived
-  bbb = 1; //sættes til 1 for at fortække at der er data der skal sendes
-  }
+  SlaveReceived = Wire.read();                                                        //Used to read value received from master and store in variable SlaveReceived
+  bbb = 1;                                                                            //sættes til 1 for at fortælle at der er data der skal sendes
+  }               
 
-void requestEvent()                                //This Function is called when Master wants value from slave
+void requestEvent()                                                                    //This Function is called when Master wants value from slave
   {
-  Wire.write(SlaveSend);                          // sends one byte converted POT value to master
+  Wire.write(SlaveSend);                                                                // sends one byte converted POT value to master
   }
